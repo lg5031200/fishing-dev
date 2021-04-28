08cdca25f1c705ee854761ff73304cd9a8f79298
Dream <dream@DreamdeMacBook-Pro.local>
2021-04-29 01:05:53 +0800

    FIX: mongodb uri

--- graphql-server/db.js
+++ graphql-server/db.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url = `mongodb://root:fishdev123@localhost:27017`;
+const dockerUri = `mongodb://root:fishdev123@localhost:27017`;
+const mongoAtlasUri =
+  'mongodb+srv://admin:polly123@cluster0.whih7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-mongoose.connect(url, { useNewUrlParser: true });
-mongoose.connection.once('open', () =>
-  console.log(`Connected to mongodb success!`)
-);
+try {
+  // Connect to the MongoDB cluster
+  mongoose.connect(
+    mongoAtlasUri || dockerUri,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => console.log(' Mongoose is connected')
+  );
+} catch (e) {
+  console.log('could not connect');
+}
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', (err) => console.log(`Connection error ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
